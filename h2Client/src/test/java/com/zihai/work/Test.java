608caa99bd83cf0c0e74fa00705bd9ad151799b8
bang.wang <bang.wang@iim.ltd>
2020-09-17 21:09:26 +0800

    init

--- h2Client/src/test/java/com/zihai/work/Test.java
+++ h2Client/src/test/java/com/zihai/work/Test.java
@@ -3,6 +3,6 @@ package com.zihai.work;
 public class Test {
     public static void main(String[] args) {
         int i = (Integer)null;
-        System.out.println(i);
+        System.out.println(i+2);
     }
 }
