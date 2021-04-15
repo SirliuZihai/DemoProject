2766050f33f84e7ae58732fc323d22a8fc986ecf
刘益之 <nicool@iim.ltd>
2021-04-15 19:05:40 +0800

    spi

--- spi/src/main/java/com/zihai/spi/service/SpiService.java
+++
@@ -1,7 +0,0 @@
-package com.zihai.spi.service;
-
-public interface SpiService {
-    String name = "test1";
-
-    String getName();
-}
--- spi/src/main/java/com/zihai/spi/service/impl/SpiServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.zihai.spi.service.impl;
-
-import com.zihai.spi.service.SpiService;
-
-public class SpiServiceImpl implements SpiService {
-    public String getName() {
-        return "陆七";
-    }
-}
